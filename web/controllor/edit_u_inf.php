9d9d388a53c47d672385815ccd7070e48529ce9d
HeYue13211114 <superheyueaaa@163.com>
2016-11-23 09:28:47 +0800

    1

---
+++ web/controllor/edit_u_inf.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "BDB";
+$conn = new mysqli ($servername, $username, $password, $dbname);
+
+if ($conn -> connect_error) {
+	$json = array ("status" => "n");
+	echo json_encode ($json);
+	exit;
+}
+$uname = "\"" . $_POST ["uname"] . "\"";	
+$upassword = "\"" . $_POST ["upassword"] . "\"";	
+$uphone = "\"" . $_POST ["uphone"] . "\"";	
+$uemail = "\"" . $_POST ["uemail"] . "\"";
+$conn -> query ("BEGIN;");
+$sql = "UPDATE U3 SET Upassword = $upassword, Uphone = $uphone, Uemail = $uemail WHERE Uname = $uname;";
+if (!($conn -> query ($sql))) {
+	$conn -> query ("ROLLBACK;");
+	$json = array ("status" => "n");
+	$conn -> close ();			
+	echo json_encode ($json);
+	exit;
+}
+$conn -> query ("COMMIT;");
+$json = array ("status" => "y");
+$conn -> close ();			
+echo json_encode ($json);
+exit;
+?>
