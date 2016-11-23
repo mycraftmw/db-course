8f5363e1614fd24fc0f138eece9fc81945e2d304
HeYue13211114 <superheyueaaa@163.com>
2016-11-23 10:54:24 +0800

    3

---
+++ web/controllor/show_cha.php
@@ -0,0 +1,47 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "BDB";
+$conn = new mysqli ($servername, $username, $password, $dbname);
+
+if ($conn -> connect_error) {
+	$json = array ("status" => "n");
+	echo json_encode ($json);
+	exit;
+}
+$gno = $_POST ["gno"];
+$sql = "SELECT Gnoplan, Gnoadopt, CHAmoney, CHAplanstate, CHAplancredit, CHAadoptstate, CHAadoptcredit, CHAtimestamp
+		FROM CHA 
+		WHERE 
+		Gnoplan = $gno OR 
+		Gnoadopt = $gno
+		ORDER BY 
+		CHAtimestamp DESC";
+$result = $conn -> query ($sql);
+if ($result) {
+	$row = $result -> fetch_assoc();
+	$json = array ("status" => "y");
+	if ($row ["Gnoplan"] == $gno) {
+		$json ["chamoney"] = $row ["CHAmoney"];
+		$json ["state"] = $row ["CHAplanstate"];
+		$json ["credit"] = $row ["CHAplancredit"];
+		$json ["chatimestamp"] = $row ["CHAtimestamp"];
+	}
+	else {
+		$json ["chamoney"] = -$row ["CHAmoney"];
+		$json ["state"] = $row ["CHAadoptstate"];
+		$json ["credit"] = $row ["CHAadoptcredit"];
+		$json ["chatimestamp"] = $row ["CHAtimestamp"];
+	}	
+	$conn -> close ();			
+	echo json_encode ($json);
+	exit;
+}
+else {
+	$json = array ("status" => "n");
+	$conn -> close ();			
+	echo json_encode ($json);
+	exit;
+}
+?>
