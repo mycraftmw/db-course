849d9cefb4d3cf0dffe2a511c0e1bc994bcdeac3
HeYue13211114 <superheyueaaa@163.com>
2016-11-23 09:35:25 +0800

    1

---
+++ web/controllor/check_r.php
@@ -0,0 +1,34 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "BDB";
+$conn = new mysqli ($servername, $username, $password, $dbname);
+
+if ($conn -> connect_error) {
+	$json = array ("status" => "n");
+	echo json_encode ($json);
+	exit;
+}
+$uname = "\"" . $_POST ["uname"] . "\""; 
+$rcontent = "\"" . $_POST ["rcontent"] . "\"";
+$sql = "SELECT *
+		FROM U1, UAR, R
+		WHERE
+		U1.Uroot = UAR.Uroot AND
+		UAR.rno = R.rno AND
+		Rcontent = $rcontent;";
+$result = $conn -> query ($sql);
+if ($result) {
+	$json = array ("status" => "y");
+	$conn -> close ();
+	echo json_encode ($json);
+	exit;
+}
+else {
+	$json = array ("status" => "n");
+	$conn -> close ();
+	echo json_encode ($json);
+	exit;
+}
+?>
