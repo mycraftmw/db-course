6ed9f137f8545f2ec254475621312f1c6176238b
HeYue13211114 <superheyueaaa@163.com>
2016-11-23 16:59:28 +0800

    3

--- web/controllor/register.php
+++ web/controllor/register.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "127.0.0.1";
 $username = "root";
-$password = "160013";
+$password = "";
 $dbname = "BDB";
 $conn = new mysqli ($servername, $username, $password, $dbname);
 
--- web/controllor/show_g.php
+++ web/controllor/show_g.php
@@ -17,24 +17,40 @@ $sql = "SELECT G1.Gno, Gname, Uname, Gtype, Gaddress, Ginstruction, Gparameter, 
 		G1.Gno = G3.Gno AND
 		G1.Gno = $gno;";
 $result = $conn -> query ($sql);
-$row = $result -> fetch_assoc();
-$json = array ("status" => "y");
-$json ["gno"] = $row ["Gno"];
-$json ["gname"] = $row ["Gname"];
-$json ["uname"] = $row ["Uname"];
-$json ["gtype"] = $row ["Gtype"];
-$json ["gaddress"] = $row ["Gaddress"];
-$json ["ginstruction"] = $row ["Ginstruction"];
-$json ["gparameter"] = $row ["Gparameter"];
-$json ["gtime"] = $row ["Gtime"];
-$json ["gprice"] = $row ["Gprice"];
-$sql = "SELECT Tno FROM DES WHERE Gno = $gno;";
-$result = $conn -> query ($sql);
-$tno = "";
-while ($row = $result -> fetch_assoc()) 
-	$tno = $row ["Tno"] . " ";
-$json ["tno"] = $tno;
-$conn -> close ();			
-echo json_encode ($json);
-exit;
+if ($result) {
+	$row = $result -> fetch_assoc();
+	$json = array ("status" => "y");
+	$json ["gno"] = $row ["Gno"];
+	$json ["gname"] = $row ["Gname"];
+	$json ["uname"] = $row ["Uname"];
+	$json ["gtype"] = $row ["Gtype"];
+	$json ["gaddress"] = $row ["Gaddress"];
+	$json ["ginstruction"] = $row ["Ginstruction"];
+	$json ["gparameter"] = $row ["Gparameter"];
+	$json ["gtime"] = $row ["Gtime"];
+	$json ["gprice"] = $row ["Gprice"];
+	$sql = "SELECT Tno FROM DES WHERE Gno = $gno;";
+	$result = $conn -> query ($sql);
+	$tno = "";
+	if ($result) {
+		while ($row = $result -> fetch_assoc()) 
+			$tno = $row ["Tno"] . " ";
+		$json ["tno"] = $tno;
+		$conn -> close ();			
+		echo json_encode ($json);
+		exit;
+	}
+	else {
+	$conn -> close ();
+	$json = array ("status" => "n");	
+	echo json_encode ($json);
+	exit;
+}
+}
+else {
+	$conn -> close ();
+	$json = array ("status" => "n");	
+	echo json_encode ($json);
+	exit;
+}
 ?>
